5aada3851d31cdaa28f80dc319be17f757498211
hafizhamza-123 <hafizhamza0044567@gmail.com>
2026-02-20 03:24:38 +0500

    Read Receipt endpoints added

--- Server/src/routes/message.route.js
+++ Server/src/routes/message.route.js
@@ -3,7 +3,12 @@ import {
   getAllMessages,
   createMessage,
   getUnreadCount, 
-  uploadMessage
+  uploadMessage, 
+  markMessagesAsDelivered,
+  markMessagesAsRead,
+  getMessageReadReceipts,
+  getMessagesWithReadStatus,
+  getChatReadSummary
 } from "../controllers/message.controller.js";
 import { authenticate } from "../middlewares/authMiddleware.js";
 import upload from "../middlewares/upload.js";
@@ -24,5 +29,20 @@ router.post("/upload/:chatId", upload.single("file"), uploadMessage);
 
 // Get unread message count 
 router.get("/:chatId/unread-count", getUnreadCount);
+
+// Mark Messages as delivered when user opens chat
+router.post('/:chatId/delivered', markMessagesAsDelivered);
+
+// Mark Messages as read when user views them
+router.put('/:chatId/read', markMessagesAsRead);
+
+// Get read receipts for a message
+router.get('/receipt/:messageId', getMessageReadReceipts);
+
+// Get messages with read status for a chat
+router.get('/:chatId/with-status', getMessagesWithReadStatus);
+
+// Get Summary of read/unread messages for a chat
+router.get('/:chatId/summary', getChatReadSummary);
 
 export default router;
