003793a66336309faa44ab2ca3b379aae9de6d7f
hafizhamza-123 <hafizhamza0044567@gmail.com>
2026-03-11 01:31:22 +0500

    UI Corrections

--- Client/src/components/chat/ChatSidebar.jsx
+++ Client/src/components/chat/ChatSidebar.jsx
@@ -65,7 +65,7 @@ export default function ChatSidebar({
         </button>
       </div>
 
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto custom-scrollbar">
         {filteredChats.map((chat) => {
           const otherUser = getOtherMember(chat);
           const isOnline = otherUser?.isOnline;
